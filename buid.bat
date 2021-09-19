485f2b232427bae95a92d9adaccb01a94b9a80e9
Martí Climent <marticlilop@gmail.com>
2021-09-19 17:37:51 +0200

    update files

--- buid.bat
+++ buid.bat
@@ -3,10 +3,11 @@ rmdir /Q /S build
 rmdir /Q /S dist
 PyInstaller __init__.py --icon icon.ico --onefile --windowed
 cd dist
-rmdir /Q /S build
-rmdir /Q /S dist
 move __init__.exe ../../
 cd ..
+rmdir /Q /S build
+rmdir /Q /S dist
 cd ..
 rename __init__.exe ElevenClock.exe
+ElevenClock.exe
 pause
