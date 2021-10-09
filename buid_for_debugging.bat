1839dd4a9f335e7d2483630ff5cc98ef0ea4230a
Martí Climent <marticlilop@gmail.com>
2021-10-09 12:11:28 +0200

    add build .bat file to bould with cmd window (debugging purposes)

---
+++ buid_for_debugging.bat
@@ -0,0 +1,14 @@
+cd elevenclock
+rmdir /Q /S build
+rmdir /Q /S dist
+PyInstaller __init__.py --icon icon.ico --add-data "icon.ico;." --onefile --clean
+cd dist
+move __init__.exe ../../
+cd ..
+rmdir /Q /S build
+rmdir /Q /S dist
+cd ..
+taskkill /im ElevenClock.exe /f
+rename __init__.exe ElevenClock.exe
+ElevenClock.exe
+pause
