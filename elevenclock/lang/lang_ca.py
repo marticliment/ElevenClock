f1963d31ca1de3718a9c8e0874c698085825b621
Martí Climent <marticlilop@gmail.com>
2021-10-18 20:49:50 +0200

    First version of the language applicator

---
+++ elevenclock/lang/lang_ca.py
@@ -0,0 +1,70 @@
+# INSTRUCTIONS
+
+# Translate the text and write it between the "
+# If you see sth like {0}, {1}, maintain it on the translated sentence
+# Meke special attention to elements like ":", etc.
+
+
+
+lang2_3 = {
+    #Context menu
+    "ElevenClock Settings"      :"Configuració de l'ElevenClock", # Also settings title
+    "Reload Clocks"             :"Recarregar l'ElevenClock",
+    "ElevenClock v{0}"          :"ElevenClock v{0}",
+    "Restart ElevenClock"       :"Reiniciar l'ElevenClock",
+    "Hide ElevenClock"          :"Amagar l'ElevenClock",
+    "Quit ElevenClock"          :"Tancar l'ElevenClock",
+    
+    #General settings section
+    "General Settings:"                                                                 :"Configuració general:",
+    "Automatically check for updates"                                                   :"Cercar actualitzacions automàticament",
+    "Automatically install available updates"                                           :"Instal·lar automàticament les actualitzacions",
+    "Enable really silent updates"                                                      :"Activar actualitzacions silencioses",
+    "Bypass update provider authenticity check (NOT RECOMMENDED, AT YOUR OWN RISK)"     :"No comprovar l'autenticitat de les actualitzacions (NO RECOMANAT, SOTA LA VOSTRA RESPONSABILITAT)",
+    "Show ElevenClock on system tray"                                                   :"Mostrar l'ElevenClock a la safata del sistema",
+    "Alternative clock alignment (may not work)"                                        :"Alineació alternativa (pot no funcionar)",
+    "Change startup behaviour"                                                          :"Canviar el comportament a l'inici",
+    "Change"                                                                            :"Canvia",
+    "<b>Update to the lastest version!</b>"                                             :"<b>Actualitza a la última versió</b>",
+    "Install update"                                                                    :"Instal·lar actualització",
+    
+    #Clock settings
+    "Clock Settings:"                                       :"Configuració del rellotge:",
+    "Hide the clock in fullscreen mode"                     :"Oculta el rellotge en mode de pantalla completa",
+    "Hide the clock when RDP client is active"              :"Oculta el rellotge si s'està fent servir el client d'Escriptori Remot",
+    "Force the clock to be at the bottom of the screen"     :"Força el rellotge a mostrar-se a la part de sota de la pantalla",
+    "Fix the hyphen/dash showing over the month"            :"Arregla el guió mostrant-se sobre el mes (format de la data)",
+    "Force the clock to have white text"                    :"Força el rellotge a tenir text blanc",
+    "Show the clock at the left of the screen"              :"Mostra el rellotge a l'esquerra de la pantalla",
+    
+    #Date & time settings
+    "Date & Time Settings:"                             :"Configuració de data i hora:",
+    "Show seconds on the clock"                         :"Mostra els segons al rellotge",
+    "Show date on the clock"                            :"Mostra la data al rellotge",
+    "Show time on the clock"                            :"Mostra l'hora al rellotge",
+    "Change date and time format (Regional settings)"   :"Canvia el format de data i hora (Configuració Regional)",
+    "Regional settings"                                 :"Configuració Regional",
+    
+    #About the language pack
+    "About the language pack:"                  :"Sobre el paquet de llengua:",
+    "Translated to English by martinet101"      :"Traduït al Català per martinet101", # Here, make sute to give you some credits:  Translated to LANGUAGE by USER/NAME/PSEUDONYM/etc. 
+    "Translate ElevenClock to your language"    :"Tradueix l'ElevenClock al teu idioma",
+    "Get started"                               :"Comença",
+    
+    #About ElevenClock
+    "About ElevenClock version {0}:"            :"Sobre l'ElevenClock versió {0}:",
+    "View ElevenClock's homepage"               :"Mostra la pàgina web de l'ElevenClock",
+    "Open"                                      :"Obre",
+    "Report an issue/request a feature"         :"Reporta un problema/proposa una característica",
+    "Report"                                    :"Reporta",
+    "Support the dev: Give me a coffee☕"       :"Suporta al desenvolupador: compra'm un cafè☕",
+    "Open page"                                 :"Obre",
+    "Icons by Icons8"                           :"Les icones són una cortesia d'Icons8", # Here, the word "Icons8" should not be translated
+    "Webpage"                                   :"Pàgina web",
+    "Close settings"                            :"Tanca la configuració",
+    "Close"                                     :"Tanca",
+}
+
+
+
+lang = lang2_3
---
+++ elevenclock/lang/lang_es.py
@@ -0,0 +1,70 @@
+# INSTRUCTIONS
+
+# Translate the text and write it between the "
+# If you see sth like {0}, {1}, maintain it on the translated sentence
+# Meke special attention to elements like ":", etc.
+
+
+
+lang2_3 = {
+    #Context menu
+    "ElevenClock Settings"      :"Configuración de ElevenClock", # Also settings title
+    "Reload Clocks"             :"Recargar ElevenClock",
+    "ElevenClock v{0}"          :"ElevenClock v{0}",
+    "Restart ElevenClock"       :"Reiniciar ElevenClock",
+    "Hide ElevenClock"          :"Ocultar ElevenClock",
+    "Quit ElevenClock"          :"Cerrar ElevenClock",
+    
+    #General settings section
+    "General Settings:"                                                                 :"Configuración general:",
+    "Automatically check for updates"                                                   :"Buscar actualizaciones automáticamente",
+    "Automatically install available updates"                                           :"Instalar automáticamente las actualizaciones",
+    "Enable really silent updates"                                                      :"Activar actualizaciones silenciosas",
+    "Bypass update provider authenticity check (NOT RECOMMENDED, AT YOUR OWN RISK)"     :"No comprobar la autenticidad de las actualizaciones (NO RECOMENDADO, BAJO VUESTRA RESPONSABILIDAD)",
+    "Show ElevenClock on system tray"                                                   :"Mostrar ElevenClock en la bandeja del sistema",
+    "Alternative clock alignment (may not work)"                                        :"Alineación alternativa (puede no funcionar)",
+    "Change startup behaviour"                                                          :"Cambiar el comportamiento al inicio",
+    "Change"                                                                            :"Cambiar",
+    "<b>Update to the lastest version!</b>"                                             :"<b>Actualiza a la última versión</b>",
+    "Install update"                                                                    :"Instalar actualización",
+    
+    #Clock settings
+    "Clock Settings:"                                       :"Configuración del reloj:",
+    "Hide the clock in fullscreen mode"                     :"Ocultar el reloj en modo de pantalla completa",
+    "Hide the clock when RDP client is active"              :"Ocultar el reloj si se está ejecutando el cliente de Escritorio Remoto",
+    "Force the clock to be at the bottom of the screen"     :"Fuerza al reloj a mostrarse a la parte inferior de la pantalla",
+    "Fix the hyphen/dash showing over the month"            :"Arregla el guión mostrandose sobre el mes (formato de la fecha)",
+    "Force the clock to have white text"                    :"Fuerza el reloj a tenir el texto blanco",
+    "Show the clock at the left of the screen"              :"Muestra el reloj a la parte izquierda de la pantalla",
+    
+    #Date & time settings
+    "Date & Time Settings:"                             :"Configuración de fecha y la hora:",
+    "Show seconds on the clock"                         :"Muestra los segundos en el reloj",
+    "Show date on the clock"                            :"Muestra la fecha en el reloj",
+    "Show time on the clock"                            :"Muestra la hora en el reloj",
+    "Change date and time format (Regional settings)"   :"Cambia el formato de la fecha y la hora (Configuración Regional)",
+    "Regional settings"                                 :"Configuración Regional",
+    
+    #About the language pack
+    "About the language pack:"                  :"Sobre el paquete de idioma:",
+    "Translated to English by martinet101"      :"Traducido al Castellano por martinet101", # Here, make sute to give you some credits:  Translated to LANGUAGE by USER/NAME/PSEUDONYM/etc. 
+    "Translate ElevenClock to your language"    :"Traduce ElevenClock a tu idioma",
+    "Get started"                               :"Empezar",
+    
+    #About ElevenClock
+    "About ElevenClock version {0}:"            :"Sobre ElevenClock versión {0}:",
+    "View ElevenClock's homepage"               :"Abre la página web de ElevenClock",
+    "Open"                                      :"Abre",
+    "Report an issue/request a feature"         :"Reporta un problema/proponer una característica",
+    "Report"                                    :"Reportar",
+    "Support the dev: Give me a coffee☕"       :"Suporta al desarrollador: cómprame un café☕",
+    "Open page"                                 :"Abre",
+    "Icons by Icons8"                           :"Los iconos son una cortesia de Icons8", # Here, the word "Icons8" should not be translated
+    "Webpage"                                   :"Página web",
+    "Close settings"                            :"Cerrar la configuración",
+    "Close"                                     :"Cerrar",
+}
+
+
+
+lang = lang2_3
