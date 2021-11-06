246267d56b556ae5dd0f4204bfc271656f137ae2
Martí Climent <marticlilop@gmail.com>
2021-11-06 21:59:16 +0100

    Fix build issue with PySide6

---
+++ build_and_debug.bat
@@ -0,0 +1,17 @@
+cd elevenclock
+rmdir /Q /S build
+rmdir /Q /S dist
+python39 -m PyInstaller __init__.py --icon icon.ico --add-data "icon.ico;." --add-data "*.png;." --add-data "lang;lang" --debug all --clean --version-file ../elevenclock-version-info --add-data "%homedrive%%homepath%\AppData\Local\Programs\Python\Python39\Lib\site-packages\PySide6\plugins;PySide6/plugins"
+cd dist
+move __init__ ../../
+cd ..
+rmdir /Q /S build
+rmdir /Q /S dist
+del __init__.spec
+cd ..
+taskkill /im ElevenClock.exe /f
+rmdir /Q /S Debug_EXE_Files
+rename __init__ Debug_EXE_Files
+cd Debug_EXE_Files
+__init__.exe
+pause
