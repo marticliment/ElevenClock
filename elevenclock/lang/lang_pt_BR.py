703aaaf2a00b35a53dd15f93a79c7fb6105c06cf
Wanderlei Hüttel <wanderleihuttel@users.noreply.github.com>
2021-12-05 20:17:27 -0300

    Create lang_pt_BR.py

---
+++ elevenclock/lang/lang_pt_BR.py
@@ -0,0 +1,115 @@
+# INSTRUCTIONS
+
+# Translate the text and write it between the "
+# EXAMPLE:      original    ->  "This text is in english: value {0}" 
+#               translation ->  "Aquest text està en anglès: valor {0}"
+# If you see sth like {0}, {1}, maintain it on the translated sentence
+# Meke special attention to elements like ":", etc.
+
+lang_2_8 = {
+    "Force the clock to be at the top of the screen": "Force the clock to be at the top of the screen",
+    "Show the clock on the primary screen": "Mostra o relógio na tela principal",
+    "Use a custom font color": "Use uma cor de fonte personalizada",
+    "Use a custom background color": "Use uma cor de fundo personalizada",
+    "Align the clock text to the center": "Alinhar o texto do relógio ao centro",
+    "Select custom color": "Selecione uma cor personalizada",
+    "Hide the clock when a program occupies all screens": "Esconder o relógio quando um programa ocupar todas as telas",
+}
+
+lang2_7_bis = lang_2_8 | {
+    "Use a custom font": Alterar o tipo da fonte",
+    "Use a custom font size": "Alterar o tamanho da fonter",
+    "Enable hide when multi-monitor fullscreen apps are running": "Esconder relógio quando aplicações multi-monitor estiverem em execução",
+    "<b>{0}</b> needs to be enabled to change this setting": "A opção <b>{0}</b> precisa de estar ativada para mudar esta definição",
+    "<b>{0}</b> needs to be disabled to change this setting": "A opção <b>{0}</b> precisa de estar desativada para mudar esta definição",
+}
+
+lang2_7 = lang2_7_bis | {
+    " (This feature has been disabled because it should work by default. If it is not, please report a bug)": "  (Funcionalidade desativada porque deve funcionar por predefinição. Caso não funcione, reporte o problema",
+    "ElevenClock's language": "Idioma ElevenClock"
+}
+
+lang2_6 = lang2_7 | {
+    "About Qt6 (PySide6)": "Acerca de Qt6 (PySide6)",
+    "About": "Sobre,
+    "Alternative non-SSL update server (This might help with SSL errors)": "Servidor de atualização alternativo sem SSL (Pode ajudar com erros de SSL)",
+    "Fixes and other experimental features: (Use ONLY if something is not working)": "Correções e funcionalidades experimentais: (Usar APENAS se algo não funcionar corretamente)",
+    "Show week number on the clock": "Mostrar dia da semana no relógio"
+}
+
+lang2_5 = lang2_6 | {
+    "Hide the clock when RDP Client or Citrix Workspace are running": "Esconder as horas quando o Ambiente de Trabalho Remoto ou o Citrix Workspace estiverem em execução",
+    "Clock Appearance:": "Aparência do relógio",
+    "Force the clock to have black text": "Forçar a cor preta para o relógio",
+    " - It is required that the Dark Text checkbox is disabled": " - É necessário que a opção acima esteja desativada",
+    "Debbugging information:": "Informação de depuração:",
+    "Open ElevenClock's log": "Abrir registro de eventos do ElevenClock",
+}
+
+lang2_4 = lang2_5 | {
+    # Added text in version 2.4
+    "Show the clock on the primary screen (Useful if clock is set on the left)": "Mostrar o relógio na tela principal (Útil se o relógio estiver definido para aparecer à esquerda)",
+    "Show weekday on the clock"  :"Mostrar o dia da semana no relógio",
+}
+
+lang2_3 = lang2_4 | {
+    #Context menu
+    "ElevenClock Settings"      :"Definições ElevenClock", # Also settings title
+    "Reload Clocks"             :"Reiniciar relógios",
+    "ElevenClock v{0}"          :"ElevenClock v{0}",
+    "Restart ElevenClock"       :"Reiniciar ElevenClock",
+    "Hide ElevenClock"          :"Esconder ElevenClock",
+    "Quit ElevenClock"          :"Sair do ElevenClock",
+    
+    #General settings section
+    "General Settings:"                                                                 :"Definições Gerais:",
+    "Automatically check for updates"                                                   :"Procurar atualizações automaticamente",
+    "Automatically install available updates"                                           :"Instalar atualizações automaticamente",
+    "Enable really silent updates"                                                      :"Ligar atualizações silenciosas",
+    "Bypass update provider authenticity check (NOT RECOMMENDED, AT YOUR OWN RISK)"     :"Não verificar integridade da atualização (NÃO RECOMENDADO, POR SUA CONTA E RISCO)",
+    "Show ElevenClock on system tray"                                                   :"Mostrar ElevenClock na área de notificação",
+    "Alternative clock alignment (may not work)"                                        :"Alinhamento alternativo (pode não funcionar)",
+    "Change startup behaviour"                                                          :"Mudar comportamento de inicialização",
+    "Change"                                                                            :"Definições",
+    "<b>Update to the latest version!</b>"                                             :"<b>Atualize para a versão mais recente!</b>",
+    "Install update"                                                                    :"Instalar atualização",
+    
+    #Clock settings
+    "Clock Settings:"                                              :"Definições do relógio:",
+    "Hide the clock in fullscreen mode"                            :"Esconder o relógio quando tela cheia",
+    "Hide the clock when RDP client is active"                     :"Esconder o relógio quando o Ambiente de Trabalho Remoto estiver a ser usado",
+    "Force the clock to be at the bottom of the screen"            :"Forçar o relógio a ficar na parte de baixo da tela",
+    "Show the clock when the taskbar is set to hide automatically" :"Mostrar o relógio quando a barra de tarefas estiver definida para se esconder automaticamente",
+    "Fix the hyphen/dash showing over the month"                   :"Corrigir problema onde aparece um hífen/traço em cima do mês",
+    "Force the clock to have white text"                           :"Forçar a cor branca para o relógio",
+    "Show the clock at the left of the screen"                     :"Mostrar relógio à esquerda da tela",
+    
+    #Date & time settings
+    "Date & Time Settings:"                             :"Definições de Data e Hora:",
+    "Show seconds on the clock"                         :"Mostrar segundos no relógio",
+    "Show date on the clock"                            :"Mostrar a data no relógio",
+    "Show time on the clock"                            :"Mostrar as horas no relógio",
+    "Change date and time format (Regional settings)"   :"Mudar formato de data e hora (Formato Regional)",
+    "Regional settings"                                 :"Definições",
+    
+    #About the language pack
+    "About the language pack:"                  :"Sobre o pacote de idiomas:",
+    "Translated to English by martinet101"      :"Traduzido para Português do Brasil por Wanderlei Huttel", # Here, make sute to give you some credits:  Translated to LANGUAGE by USER/NAME/PSEUDONYM/etc. 
+    "Translate ElevenClock to your language"    :"Traduzir o ElevenClock para o seu idioma",
+    "Get started"                               :"Traduzir",
+    
+    #About ElevenClock
+    "About ElevenClock version {0}:"            :"Sobre a versão {0} do ElevenClock:",
+    "View ElevenClock's homepage"               :"Ver página inicial do ElevenClock",
+    "Open"                                      :"Abrir",
+    "Report an issue/request a feature"         :"Reportar um problema/pedir uma funcionalidade",
+    "Report"                                    :"Reportar",
+    "Support the dev: Give me a coffee☕"       :"Apoia o desenvolvedor: Me pague um café☕",
+    "Open page"                                 :"Abrir página",
+    "Icons by Icons8"                           :"Ícones por Icons8", # Here, the word "Icons8" should not be translated
+    "Webpage"                                   :"Website",
+    "Close settings"                            :"Fechar definições",
+    "Close"                                     :"Fechar",
+}
+
+lang = lang2_3
