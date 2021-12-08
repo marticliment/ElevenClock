ab7272721ccc2cadc818d3751b4eed69aeb66145
Martí Climent <marticlilop@gmail.com>
2021-12-08 10:22:59 +0100

    Update Portuguese translation to be able to add portughese (brazil translation)

--- elevenclock/languages.py
+++ elevenclock/languages.py
@@ -1,5 +1,5 @@
 from lang import lang_de, lang_fr, lang_ca, lang_es, lang_ru, lang_en, lang_tr, lang_pl, lang_it, lang_nl, lang_nb, lang_ko, lang_vi, lang_el, lang_zh_TW
-from lang import lang_zh_CN, lang_pt, lang_ja, lang_fi, lang_id, lang_sr, lang_lt, lang_sv, lang_ua, lang_da, lang_lv, lang_hu, lang_sk
+from lang import lang_zh_CN, lang_pt_PT, lang_ja, lang_fi, lang_id, lang_sr, lang_lt, lang_sv, lang_ua, lang_da, lang_lv, lang_hu, lang_sk
 
 lang = None
 
@@ -22,7 +22,7 @@ languages = {
     "nb": lang_nb,
     "nl": lang_nl,
     "pl": lang_pl,
-    "pt": lang_pt,
+    "pt_PT": lang_pt_PT,
     "ru": lang_ru,
     "sk": lang_sk,
     "sr": lang_sr,
@@ -53,7 +53,7 @@ languageReference = {
     "lt": "Lithuanian",
     "nb": "Norwegian",
     "pl": "Polish" ,
-    "pt": "Portuguese",
+    "pt_PT": "Portuguese (Portugal)",
     "ru": "Russian",
     "zh_CN": "Simplified Chinese (China)",
     "sr": "Serbian",
