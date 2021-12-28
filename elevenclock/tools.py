ddce4dc841c1d3d08b30c7c80eccf5d78f80ee9d
Martí Climent <marticlilop@gmail.com>
2021-12-28 14:34:26 +0100

    Update tools.py

--- elevenclock/tools.py
+++ elevenclock/tools.py
@@ -11,8 +11,8 @@ from PySide2.QtWidgets import *
 import globals
 from languages import *
 
-version = 2.92
-versionName = "2.9.2"
+version = 3
+versionName = "3.0.0-beta"
 
 def _(s): #Translate function
     global lang
