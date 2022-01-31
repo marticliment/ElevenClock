2b36b97991f78c2eb0c6c646d9454ff140ed5094
Joost Laoh <joost.swag1@gmail.com>
2022-01-31 11:48:29 +0100

    Updated lang_nl.py

--- elevenclock/lang/lang_nl.py
+++ elevenclock/lang/lang_nl.py
@@ -8,123 +8,123 @@
 
 lang_3_1 = {
     "W": "", # The initial of the word week in your language: W for week, S for setmana, etc.
-    "Disable the notification badge": "",
-    "Override clock default height": "",
-    "Adjust horizontal clock position": "",
-    "Adjust vertical clock position": "",
-    "Export log as a file": "",
-    "Copy log to clipboard": "",
-    "Announcements:": "",
-    "Fetching latest announcement, please wait...": "",
-    "Couldn't load the announcements. Please try again later": "",
+    "Disable the notification badge": "Verberg de meldingsbadge",
+    "Override clock default height": "Overschrijf standaard klok hoogte",
+    "Adjust horizontal clock position": "Pas de horizontale klok positie aan",
+    "Adjust vertical clock position": "Pas de verticale klok positie aan",
+    "Export log as a file": "Exporteer log als bestand",
+    "Copy log to clipboard": "Kopieer log naar klembord",
+    "Announcements:": "Aankondigingen:",
+    "Fetching latest announcement, please wait...": "Laatste aankondiging ophalen, wacht alsjeblieft...",
+    "Couldn't load the announcements. Please try again later": "Kon de aankondigingen niet laden. Probeer het later opnieuw",
     "ElevenClock's log": "",
-    "Pick a color": ""
+    "Pick a color": "Kies een kleur"
 }
 
 lang_3 = lang_3_1 | {
-    "Hide the clock during 10 seconds when clicked": "",
-    "Enable low-cpu mode": "",
-    "You might lose functionalities, like the notification counter or the dynamic background": "",
-    "Clock position and size:": "",
-    "Clock size preferences, position offset, clock at the left, etc.": "",
+    "Hide the clock during 10 seconds when clicked": "Verberg de klok gedurende 10 seconden wanneer erop wordt geklikt",
+    "Enable low-cpu mode": "Lage cpu-modus inschakelen",
+    "You might lose functionalities, like the notification counter or the dynamic background": "Mogelijk verliest u functionaliteiten, zoals de notificatieteller of de dynamische achtergrond",
+    "Clock position and size:": "Klokpositie en -grootte:",
+    "Clock size preferences, position offset, clock at the left, etc.": "Klokgrootte voorkeuren, positie-offset, klok aan de linkerkant, etc.",
     "Reset monitor blacklisting status": "",
     "Reset": "",
-    "Third party licenses": "",
-    "View": "",
+    "Third party licenses": "Licenties van derden ",
+    "View": "Toon",
     "ElevenClock": "",
     "Monitor tools": "",
-    "Blacklist this monitor": "",
-    "Third Party Open-Source Software in Elevenclock {0} (And their licenses)": "",
-    "ElevenClock is an Open-Source application made with the help of other libraries made by the community:": "",
+    "Blacklist this monitor": "Blacklist deze monitor",
+    "Third Party Open-Source Software in Elevenclock {0} (And their licenses)": "Open source-software van derden in Elevenclock {0} (En hun licenties)",
+    "ElevenClock is an Open-Source application made with the help of other libraries made by the community:": "ElevenClock is een Open-Source applicatie die is gemaakt met de hulp van andere bibliotheken die door de gemeenschap zijn gemaakt:", # iffy translation, might need to be looked at
     "Ok": "",
-    "More Info": "",
-    "About Qt": "",
-    "Success": "",
-    "The monitors were unblacklisted successfully.": "",
-    "Now you should see the clock everywhere": "",
+    "More Info": "Meer Info",
+    "About Qt": "Over Qt",
+    "Success": "Succes",
+    "The monitors were unblacklisted successfully.": "De monitoren zijn met succes van de blacklist gehaald.",
+    "Now you should see the clock everywhere": "Nu zou je de klok overal moeten zien",
     "Ok": "",
     "Blacklist Monitor": "",
-    "Blacklisting a monitor will hide the clock on this monitor permanently.": "",
-    "This action can be reverted from the settings window. under <b>Clock position and size</b>": "",
-    "Are you sure do you want to blacklist the monitor \"{0}\"?": "",
-    "Yes": "",
-    "No": "",
+    "Blacklisting a monitor will hide the clock on this monitor permanently.": "Als u een monitor op de blacklist zet, wordt de klok op deze monitor permanent verborgen.",
+    "This action can be reverted from the settings window. under <b>Clock position and size</b>": "Deze actie kan worden teruggedraaid vanuit het instellingenvenster. Onder <b>Klokpositie en -grootte</b>",
+    "Are you sure do you want to blacklist the monitor \"{0}\"?": "Weet u zeker dat u de monitor \"{0}\" op de blacklist wilt zetten?",
+    "Yes": "Ja",
+    "No": "Nee",
 }
 
 lang_2_9_2 = lang_3 | {
-    "Reload log": "",
-    "Do not show the clock on secondary monitors": "",
-    "Disable clock taskbar background color (make clock transparent)": "",
-    "Open the welcome wizard": "",
-    " (ALPHA STAGE, MAY NOT WORK)": "",
-    "Welcome to ElevenClock": "",
+    "Reload log": "Log opnieuw laden",
+    "Do not show the clock on secondary monitors": "Toon de klok niet op secundaire monitoren",
+    "Disable clock taskbar background color (make clock transparent)": "Taakbalkachtergrondkleur klok uitschakelen (klok transparant maken)",
+    "Open the welcome wizard": "Open de welkomstwizard",
+    " (ALPHA STAGE, MAY NOT WORK)": " (ALFA FASE, WERKT MOGELIJK NIET)",
+    "Welcome to ElevenClock": "Welkom bij ElevenClock",
     "Skip": "",
     "Start": "",
-    "Next": "",
+    "Next": "Volgende",
     "Finish": "",
 }
 
 lang_2_9 = lang_2_9_2 | {
-    "Task Manager": "",
-    "Change date and time": "",
-    "Notification settings": "",
-    "Updates, icon tray, language": "",
-    "Hide extended options from the clock right-click menu (needs a restart to be aplied)": "",
-    "Fullscreen behaviour, clock position, 1st monitor clock, other miscellanious settings": "",
-    'Add the "Show Desktop" button on the left corner of every clock': '',
-    'You might need to set a custom background color for this to work.&nbsp;More info <a href="{0}" style="color:DodgerBlue">HERE</a>': '',
-    "Clock's font, font size, font color and background, text alignment": "",
-    "Date format, Time format, seconds,weekday, weeknumber, regional settings": "",
-    "Testing features and error-fixing tools": "",
-    "Language pack author(s), help translating ElevenClock": "",
-    "Info, report a bug, submit a feature request, donate, about": "",
-    "Log, debugging information": "",
+    "Task Manager": "Taakbeheer",
+    "Change date and time": "Datum en tijd wijzigen",
+    "Notification settings": "Melding instellingen",
+    "Updates, icon tray, language": "Updates, icoon lade, taal",
+    "Hide extended options from the clock right-click menu (needs a restart to be aplied)": "Verberg uitgebreide opties in het rechtsklikmenu van de klok (hiervoor is een herstart nodig)",
+    "Fullscreen behaviour, clock position, 1st monitor clock, other miscellanious settings": "Volledig scherm gedrag, klokpositie, 1e monitor klok, overige instellingen",
+    'Add the "Show Desktop" button on the left corner of every clock': 'Voeg de "Bureaublad weergeven" knop toe in de linkerhoek van elke klok',
+    'You might need to set a custom background color for this to work.&nbsp;More info <a href="{0}" style="color:DodgerBlue">HERE</a>': 'Mogelijk moet u een aangepaste achtergrondkleur instellen om dit te laten werken.&nbsp;Meer info <a href="{0}" style="color:DodgerBlue">HIER</a>',
+    "Clock's font, font size, font color and background, text alignment": "Lettertype van de klok, lettergrootte, letterkleur en -achtergrond, tekstuitlijning",
+    "Date format, Time format, seconds,weekday, weeknumber, regional settings": "Datumformaat, Tijdformaat, seconden, weekdag, weeknummer, regionale instellingen",
+    "Testing features and error-fixing tools": "Functies testen en hulpprogramma's voor het oplossen van fouten",
+    "Language pack author(s), help translating ElevenClock": "Auteur(s) van het taalpakket, help het vertalen van ElevenClock",
+    "Info, report a bug, submit a feature request, donate, about": "Info, een bug melden, een functieverzoek indienen, doneren, over",
+    "Log, debugging information": "Log, foutopsporingsinformatie",
 }
 
 lang_2_8 = lang_2_9 | {
-    "Force the clock to be at the top of the screen": "",
-    "Show the clock on the primary screen": "",
-    "Use a custom font color": "",
-    "Use a custom background color": "",
-    "Align the clock text to the center": "",
-    "Select custom color": "",
-    "Hide the clock when a program occupies all screens": "",
+    "Force the clock to be at the top of the screen": "Forceer de klok om bovenaan het scherm te staan",
+    "Show the clock on the primary screen": "Toon de klok op het primaire scherm",
+    "Use a custom font color": "Gebruik een aangepaste letterkleur",
+    "Use a custom background color": "Gebruik een aangepaste achtergrondkleur",
+    "Align the clock text to the center": "Lijn de kloktekst uit met het midden",
+    "Select custom color": "Selecteer aangepaste kleur",
+    "Hide the clock when a program occupies all screens": "Verberg de klok wanneer een programma alle schermen gebruikt",
 }
 
 lang2_7_bis = lang_2_8 | {
-    "Use a custom font": "",
-    "Use a custom font size": "",
-    "Enable hide when multi-monitor fullscreen apps are running": "",
-    "<b>{0}</b> needs to be enabled to change this setting": "",
-    "<b>{0}</b> needs to be disabled to change this setting": "",
+    "Use a custom font": "Gebruik een aangepast lettertype",
+    "Use a custom font size": "Gebruik een aangepaste lettergrootte",
+    "Enable hide when multi-monitor fullscreen apps are running": "Verberg wanneer apps op volledig scherm met meerdere monitoren actief zijn",
+    "<b>{0}</b> needs to be enabled to change this setting": "<b>{0}</b> moet zijn ingeschakeld om deze instelling te wijzigen",
+    "<b>{0}</b> needs to be disabled to change this setting": "<b>{0}</b> moet zijn uitgeschakeld om deze instelling te wijzigen",
 }
 
 lang2_7 = lang2_7_bis | {
-    " (This feature has been disabled because it should work by default. If it is not, please report a bug)": "",
-    "ElevenClock's language": ""
+    " (This feature has been disabled because it should work by default. If it is not, please report a bug)": " (Deze functie is uitgeschakeld omdat deze standaard zou moeten werken. Als dit niet het geval is, meldt u een bug)",
+    "ElevenClock's language": "ElevenClock zijn taal"
 }
 
 lang2_6 = lang2_7 | {
-    "About Qt6 (PySide6)": "",
-    "About": "",
-    "Alternative non-SSL update server (This might help with SSL errors)": "",
-    "Fixes and other experimental features: (Use ONLY if something is not working)": "",
-    "Show week number on the clock": "",
+    "About Qt6 (PySide6)": "Over Qt6 (PySide6)",
+    "About": "Over",
+    "Alternative non-SSL update server (This might help with SSL errors)": "Alternatieve niet-SSL-updateserver (dit kan helpen bij SSL-fouten)",
+    "Fixes and other experimental features: (Use ONLY if something is not working)": "Oplossingen en andere experimentele functies: (ALLEEN gebruiken als iets niet werkt)",
+    "Show week number on the clock": "Toon weeknummer op de klok",
 }
 
 lang2_5 = lang2_6 | {
-    "Hide the clock when RDP Client or Citrix Workspace are running": "",
-    "Clock Appearance:": "",
-    "Force the clock to have black text": "",
-    " - It is required that the Dark Text checkbox is disabled": "",
-    "Debbugging information:": "",
-    "Open ElevenClock's log": "",
+    "Hide the clock when RDP Client or Citrix Workspace are running": "Verberg de klok wanneer RDP Client of Citrix Workspace actief zijn",
+    "Clock Appearance:": "Klok Uiterlijk",
+    "Force the clock to have black text": "Forceer de klok om zwarte tekst te hebben",
+    " - It is required that the Dark Text checkbox is disabled": "- Het is vereist dat het selectievakje Donkere tekst is uitgeschakeld",
+    "Debbugging information:": "Foutopsporingsinformatie",
+    "Open ElevenClock's log": "Open de log van ElevenClock",
 }
 
 lang2_4 = lang2_5 | {
     # Added text in version 2.4
-    "Show the clock on the primary screen (Useful if clock is set on the left)": "",
-    "Show weekday on the clock"  :"",
+    "Show the clock on the primary screen (Useful if clock is set on the left)": "Toon de klok op het primaire scherm (handig als de klok aan de linkerkant is ingesteld)",
+    "Show weekday on the clock"  :"Toon weekdag op de klok",
 }
 
 lang2_3 = lang2_4 | {
@@ -169,7 +169,7 @@ lang2_3 = lang2_4 | {
     
     #About the language pack
     "About the language pack:"                  :"Over het taalpakket:",
-    "Translated to English by martinet101"      :"Vertaald naar het Nederlands door Bugs", # Here, make sute to give you some credits:  Translated to LANGUAGE by USER/NAME/PSEUDONYM/etc. 
+    "Translated to English by martinet101"      :"Vertaald naar het Nederlands door Bugs en joosthoi1", # Here, make sute to give you some credits:  Translated to LANGUAGE by USER/NAME/PSEUDONYM/etc. 
     "Translate ElevenClock to your language"    :"Vertaal ElevenClock naar jou taal",
     "Get started"                               :"Begin",
     
